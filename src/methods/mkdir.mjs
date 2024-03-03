2c9773f03a34394376ebcb2bec8516d4967d3bd4
dasdeo <mark@anio.software>
2024-03-03 18:17:45 +0100

    Add options parameter to mkdir.

--- src/methods/mkdir.mjs
+++ src/methods/mkdir.mjs
@@ -1,9 +1,17 @@
-function sync_impl(fs, path) {
-	fs.mkdirSync(path)
+function sync_impl(fs, path, options = null) {
+	if (options === null) {
+		fs.mkdirSync(path)
+	} else {
+		fs.mkdirSync(path, options)
+	}
 }
 
-async function async_impl(fs, path) {
-	await fs.promises.mkdir(path)
+async function async_impl(fs, path, options = null) {
+	if (options === null) {
+		await fs.promises.mkdir(path)
+	} else {
+		await fs.promises.mkdir(path, options)
+	}
 }
 
 export default {sync_impl, async_impl}
