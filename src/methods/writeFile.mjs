b8bf985d428c57fafefcc51466f290920893c6af
dasdeo <mark@anio.software>
2024-03-04 23:40:04 +0100

    Fix writeFile().

--- src/methods/writeFile.mjs
+++ src/methods/writeFile.mjs
@@ -1,9 +1,9 @@
-function sync_impl(fs, path) {
-	return fs.writeFileSync(path)
+function sync_impl(fs, path, data) {
+	return fs.writeFileSync(path, data)
 }
 
 async function async_impl(fs, path) {
-	return await fs.promises.writeFile(path)
+	return await fs.promises.writeFile(path, data)
 }
 
 export default {sync_impl, async_impl}
